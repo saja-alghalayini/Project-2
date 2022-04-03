3e16fcac1ba5c0c9b49677ffb6b595ac1c468c66
Saja Al Ghalayini <saja.ghalayini02@gmail.com>
2022-04-03 22:45:05 +0300

    prjct2

---
+++ QuizzesPG/JavaScript/js.js
@@ -0,0 +1,27 @@
+
+'use strict'
+
+// var countDownDate = new setMinutes("00:10:00").getTime(); //Time we're counting down to//
+
+
+// var x = setInterval(function() // Update the count down every 1 second//
+// {
+
+//      var now = new Date().getTime(); //today's date and time//
+
+     
+//      var distance = countDownDate - now;  //distance between now and the count down date//
+  
+
+//      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); //Time calculations foe min & sec//
+//      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+//      document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s "; // Display result//    
+//      if (distance < 0) {
+//        clearInterval(x);
+//        document.getElementById("demo").innerHTML = " Times Up!! "; //some text to apper when the timer is stopped//
+//      }
+
+
+// }, 1000);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Project-2
+# Project 2
+
+This Project was given to us by Orange Coding Academy, We worked on it as a group...
+we built a responsive and dynamic quiz website that  requires the user 
+to register and sign in to answer  quiz questions, the user can also 
+compare the answers with the correct answers, which was previously saved on local storage...
+
+
+![C:\Users\dell\Project-2\img\Layer_52.png]()
+
+## Our Group :
+
+### Hadi AlSaden
+hadialsaden@gmail.com
+#### [Hadi's GitHub Link](https://github.com/Hadi-AL-Saden)
+
+### Saja Al Ghalayini
+saja.ghalayini02@gmail.com
+#### [Saja's GitHub Link](https://github.com/saja-alghalayini)
+
+
+### Samar Alkhamis
+ SamarAlkhamiss@gmail.com
+#### [Samar's GitHub Link](https://github.com/samaralkhamis)
